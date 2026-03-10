c7a02c4cd75826745f598ac67c557227488bf235
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-03-10 08:29:44 -0300

    Finaliza tipagem de users

--- src/contexts/users/users.tsx
+++ src/contexts/users/users.tsx
@@ -1,98 +1,109 @@
-import { createContext, useContext } from 'react';
-import authStorage from 'simple-auth-storage';
-import type { User, UserLogin, UserRegister } from '../../types';
+import { createContext, useContext } from "react";
+import authStorage from "simple-auth-storage";
+import type { User, UserLogin, UserRegister } from "../../types";
+import { fetchData } from "../../utils";
 
 const UsersContext = createContext<{
-	createUser: (user: UserRegister) => Promise<void>;
-	validateUser: (userLogin: UserLogin) => Promise<User | null>;
-	updateUser: (user: User) => Promise<void>;
-	getUser: () => User | null;
-	isAuthenticated: () => boolean;
-	login: (userLogin: UserLogin) => Promise<User | null>;
+  createUser: (user: UserRegister) => Promise<User | null>;
+  validateUser: (userLogin: UserLogin) => Promise<User | null>;
+  updateUser: (user: User) => Promise<User | null>;
+  getUser: () => User | null;
+  isAuthenticated: () => boolean;
+  login: (userLogin: UserLogin) => Promise<User | null>;
 }>({
-	createUser: async () => {},
-	validateUser: async () => null,
-	updateUser: async () => {},
-	getUser: () => null,
-	isAuthenticated: () => false,
-	login: async () => null,
+  createUser: async () => null,
+  validateUser: async () => null,
+  updateUser: async () => null,
+  getUser: () => null,
+  isAuthenticated: () => false,
+  login: async () => null,
 });
 
 export const UsersProvider = ({ children }: { children: React.ReactNode }) => {
-	const fetchUsers = async () => {
-		const response = await fetch('http://localhost:3001/users');
-		const data = await response.json();
-		return data;
-	};
+  const fetchUsers = async () => {
+    const data = await fetchData<User[]>("http://localhost:3001/users");
+    return data;
+  };
 
-	const createUser = async (user: UserRegister) => {
-		const checkForDuplicate = await fetchUsers();
-		const isDuplicate = checkForDuplicate.some((u: User) => u.email === user.email);
-		if (isDuplicate) {
-			throw new Error('User already exists');
-		}
-		const response = await fetch('http://localhost:3001/users', {
-			method: 'POST',
-			body: JSON.stringify(user),
-		});
-		const data = await response.json();
-		const token = authStorage.generateToken(data);
-		authStorage.saveUser(data, token);
-		return data;
-	};
+  const createUser = async (user: UserRegister) => {
+    const checkForDuplicate = await fetchUsers();
+    const isDuplicate = checkForDuplicate.some(
+      (u: User) => u.email === user.email,
+    );
+    if (isDuplicate) {
+      throw new Error("User already exists");
+    }
+    const data = await fetchData<User, UserRegister>(
+      "http://localhost:3001/users",
+      "POST",
+      user,
+    );
+    const token = authStorage.generateToken(data);
+    authStorage.saveUser(data, token);
+    return data;
+  };
 
-	const validateUser = async (UserLogin: UserLogin) => {
-		const users = await fetchUsers();
-		const userFound = users.find((user: UserLogin) => user.email === UserLogin.email && user.password === UserLogin.password);
-		if (userFound) {
-			return userFound;
-		}
-		return null;
-	};
+  const validateUser = async (UserLogin: UserLogin) => {
+    const users = await fetchUsers();
+    const userFound = users.find(
+      (user: User) => user.email === UserLogin.email,
+    );
+    if (userFound) {
+      return userFound;
+    }
+    return null;
+  };
 
-	const updateUser = async (user: User) => {
-		const response = await fetch(`http://localhost:3001/users/${user.id}`, {
-			method: 'PUT',
-			body: JSON.stringify(user),
-		});
-		const data = await response.json();
-		const token = authStorage.generateToken();
-		authStorage.saveUser(data, token);
-		return data;
-	};
+  const updateUser = async (user: User) => {
+    const data = await fetchData<User, User>(
+      `http://localhost:3001/users/${user.id}`,
+      "PUT",
+      user,
+    );
+    const token = authStorage.generateToken();
+    authStorage.saveUser(data, token);
+    return data;
+  };
 
-	const login = async (UserLogin: UserLogin) => {
-		const user = await validateUser(UserLogin);
-		if (user) {
-			const token = authStorage.generateToken(user);
-			authStorage.saveUser(user, token);
-		}
-		return user;
-	};
+  const login = async (UserLogin: UserLogin) => {
+    const user = await validateUser(UserLogin);
+    if (user) {
+      const token = authStorage.generateToken(user);
+      authStorage.saveUser(user, token);
+    }
+    return user;
+  };
 
-	const getUser = () => {
-		const user = authStorage.getUser();
-		return user;
-	};
+  const getUser = () => {
+    const user: User | null = authStorage.getUser();
+    return user;
+  };
 
-	const isAuthenticated = () => {
-		const isLoggedIn = authStorage.isLoggedIn();
-		return isLoggedIn;
-	};
+  const isAuthenticated = () => {
+    const isLoggedIn: boolean = authStorage.isLoggedIn();
+    return isLoggedIn;
+  };
 
-	return (
-		<UsersContext.Provider
-			value={{ createUser, validateUser, updateUser, getUser, login, isAuthenticated }}
-		>
-			{children}
-		</UsersContext.Provider>
-	);
+  return (
+    <UsersContext.Provider
+      value={{
+        createUser,
+        validateUser,
+        updateUser,
+        getUser,
+        login,
+        isAuthenticated,
+      }}
+    >
+      {children}
+    </UsersContext.Provider>
+  );
 };
 
 export const useUsers = () => {
-	const context = useContext(UsersContext);
-	if (!context) {
-		throw new Error('useUsers must be used within a UsersProvider');
-	}
-	return context;
+  const context = useContext(UsersContext);
+  if (!context) {
+    throw new Error("useUsers must be used within a UsersProvider");
+  }
+  return context;
 };
