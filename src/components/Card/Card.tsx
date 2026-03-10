74b2a6bcaffdc84d759eb808c72ae3d121ed0876
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-03-10 09:01:18 -0300

    finaliza configurações de tipagem]

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router";
 import type { Pet } from "../../types";
 
-const Card = ( { pet }: { pet: Pet } ) => {
+const Card = ({ pet }: { pet: Pet }) => {
   return (
-    <div className="bg-[#F6F6F6] rounded-lg shadow-md p-6 w-full flex flex-col items-center justify-center">
+    <div className="bg-[#f1eaea] rounded-lg shadow-md p-6 w-full flex flex-col items-center justify-center">
       <img
         src={pet.image}
         alt={pet.name}
